0ad98cb751c8039b1c7cdc7447a563ad1ad3a3be
KanishAnand <kanish.anand@students.iiit.ac.in>
2021-10-31 02:52:19 +0530

    1269D.cpp

---
+++ 1269D.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag,
+//              tree_order_statistics_node_update>
+//     new_data_set;
+// // for multiset
+// // typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+// //              tree_order_statistics_node_update>
+// //     new_data_set;
+// order_of_key(val): returns the number of values less than val
+// find_by_order(k): returns an iterator to the kth largest element (0-based)
+#define lli long long int
+#define llu unsigned long long int
+#define fr(va, beg, end) for (lli va = beg; va < end; va++)
+#define pb push_back
+#define rt return 0
+#define endln "\n"
+#define all(x) x.begin(), x.end()
+#define sz(x) (lli)(x.size())
+// cout << fixed << setprecision(0) << pi <<" "<<npi<<endl;
+
+lli input(lli n, vector<lli> &ve);
+lli input(lli n, vector<lli> &ve) {
+    lli val;
+    for (lli i = 0; i < n; i++) {
+        cin >> val;
+        ve.pb(val);
+    }
+    return 0;
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    lli n, a;
+    vector<lli> v;
+    cin >> n;
+    lli w = 0, b = 0;
+    fr(i, 0, n) {
+        cin >> a;
+        if (i % 2 == 0) {
+            w += a / 2 + a % 2;
+            b += a / 2;
+        } else {
+            b += a / 2 + a % 2;
+            w += a / 2;
+        }
+    }
+    cout << min(w, b) << endl;
+    rt;
+}
